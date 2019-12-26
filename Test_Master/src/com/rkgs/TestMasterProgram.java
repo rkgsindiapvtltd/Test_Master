1ec4f3d4efc28501750d1dd532bd5dd3a243b2b4
ROHITH <ROHITH@ROHITHSAICHIGURUPATI>
2019-12-26 23:07:45 +0530

    Changed TestMasterProgram 26122019

--- Test_Master/src/com/rkgs/TestMasterProgram.java
+++ Test_Master/src/com/rkgs/TestMasterProgram.java
@@ -4,6 +4,9 @@ public class TestMasterProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Test_Master");
+		if (args[0]==null) {
+			System.out.println("Null Argument");
+		}
 	}
 
 }
