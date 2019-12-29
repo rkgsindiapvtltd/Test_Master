68dc626ae7bc59acc31e65e70c002bacc711ce08
ROHITH <ROHITH@ROHITHSAICHIGURUPATI>
2019-12-29 11:50:27 +0530

    Added SampleClass and Changed TestMasterCls

---
+++ Test_Master/src/com/rkgs/SampleMan.java
@@ -0,0 +1,20 @@
+package com.rkgs;
+
+public class SampleMan {
+
+	private int Lid;
+	private String Lname;
+	
+	public int getLid() {
+		return Lid;
+	}
+	public void setLid(int lid) {
+		Lid = lid;
+	}
+	public String getLname() {
+		return Lname;
+	}
+	public void setLname(String lname) {
+		Lname = lname;
+	}
+}
--- Test_Master/src/com/rkgs/TestMasterProgram.java
+++ Test_Master/src/com/rkgs/TestMasterProgram.java
@@ -3,7 +3,9 @@ package com.rkgs;
 public class TestMasterProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Test_Master");
+		SampleMan sm =new SampleMan();
+		sm.setLid(1);
+		sm.setLname("Raam");
 	}
 
 }
